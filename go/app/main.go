42a4cbb75d95d7c83f37068085f39a679675c2ce
Al-Mikan <nekomimi987@gmail.com>
2022-05-17 17:13:31 +0900

    3-2,3-3

--- go/app/main.go
+++ go/app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
@@ -16,6 +17,15 @@ const (
 	ImgDir = "image"
 )
 
+type item struct {
+	Name     string `json:"name"`
+	Category string `json:"category"`
+}
+
+type itemlist struct {
+	Items []item `json:"items"`
+}
+
 type Response struct {
 	Message string `json:"message"`
 }
@@ -24,22 +34,51 @@ func root(c echo.Context) error {
 	res := Response{Message: "Hello, world!"}
 	return c.JSON(http.StatusOK, res)
 }
+func getItems(c echo.Context) error {
+	//jsonをGoに持ってきている
+	jsonFromFile, err := os.ReadFile("./items.json")
+
+	if err != nil {
+		c.Logger().Error("Notfound ./items.json")
+	}
+	//jsonを構造体に変換
+	var els itemlist
+	err = json.Unmarshal(jsonFromFile, &els)
+
+	return c.JSON(http.StatusOK, els)
+}
 
 func addItem(c echo.Context) error {
 	// Get form data
 	name := c.FormValue("name")
-	c.Logger().Infof("Receive item: %s", name)
+	category := c.FormValue("category")
+	newItem := item{Name: name, Category: category}
+	//jsonをGoに持ってきている
+	jsonFile, err := os.ReadFile("./items.json")
+	//error処理
+	if err != nil {
+		c.Logger().Error("Notfound ./items.json")
+	}
 
-	message := fmt.Sprintf("item received: %s", name)
-	res := Response{Message: message}
+	var els itemlist
+	//jsonを構造体に変換
+	err = json.Unmarshal(jsonFile, &els)
+	if err != nil {
+		c.Logger().Error("error occured while unmarshalling json")
+	}
+	//els.Itemsに新しく追加
+	els.Items = append(els.Items, newItem)
+	file, _ := json.MarshalIndent(els, "", " ")
+	os.WriteFile("./items.json", file, 0644)
 
-	return c.JSON(http.StatusOK, res)
+	message := fmt.Sprintf("item received: %s,category: %s", name, category)
+
+	return c.JSON(http.StatusOK, Response{Message: message})
 }
 
 func getImg(c echo.Context) error {
 	// Create image path
 	imgPath := path.Join(ImgDir, c.Param("itemImg"))
-
 	if !strings.HasSuffix(imgPath, ".jpg") {
 		res := Response{Message: "Image path does not end with .jpg"}
 		return c.JSON(http.StatusBadRequest, res)
@@ -63,6 +102,7 @@ func main() {
 	if front_url == "" {
 		front_url = "http://localhost:3000"
 	}
+
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{front_url},
 		AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
@@ -70,6 +110,7 @@ func main() {
 
 	// Routes
 	e.GET("/", root)
+	e.GET("/items", getItems)
 	e.POST("/items", addItem)
 	e.GET("/image/:itemImg", getImg)
 
